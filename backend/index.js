a1c3d7bc34f1b8c1b525bf4a0c5645570628a6ff
Simran <simransia07@gmail.com>
2022-06-05 13:46:22 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use('/api/notes', require('./routes/notes'))
 // --------------------------deployment------------------------------
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname+'../build')));
+  app.use(express.static(path.join(__dirname,'..', "build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname,'..', "build", "index.html"))
